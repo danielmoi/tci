adcb9ce2a28bdbb7f4c6c9d48aaf1bb1a07f20dc
danielmoi <dmoixy@gmail.com>
2023-09-18 09:29:44 +1000

    Remove template readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import type { Component } from 'solid-js';
+import Eleveld from './Eleveld';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
@@ -7,18 +8,7 @@ const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class="text-3xl font-bold underline"
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <Eleveld />
       </header>
     </div>
   );
---
+++ src/Eleveld.tsx
@@ -0,0 +1,53 @@
+import { createSignal } from "solid-js";
+
+type HTMLElementEvent<T extends HTMLElement> = Event & {
+  target: T;
+}
+
+function Eleveld() {
+  const [count, setCount] = createSignal(1);
+  const increment = () => setCount(count() + 1);
+
+  const [age, setAge] = createSignal('0');
+  const handleInput = (e: HTMLElementEvent<HTMLInputElement>) => {
+    const val = (e.target.value);
+    setAge(val);
+  }
+  const getBIS60 = () => {
+    return (2.2 - (Number(age()) * 0.01)).toFixed(1);
+  }
+  const getBIS46 = () => {
+    return (4.0 - (Number(age()) * 0.01)).toFixed(1);
+  }
+
+  const handleReset = () => setAge('0')
+
+  return (
+    <div>
+      <h1 class="mb-10">Eleveld Cet calculator</h1>
+      <div class="mb-10">
+        Enter age:
+        <input class="text-black ml-4 pl-4 w-24" type="number" onInput={handleInput} value={age()} size="5"/>
+      </div>
+      <div>BIS 60: {getBIS60()}</div>
+      <div>BIS 46: {getBIS46()}</div>
+
+      <div class="mt-10">
+        <button type="button" class="bg-slate-500 hover:bg-slate-700 text-white py-2 px-4 rounded" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Eleveld;
+
+
+/*
+
+BIS 46: Cet = 4-(age x 0.02) ie 4 - 0.2 per decade age
+
+BIS 60: Cet = 2.2-(age x 0.01) ie 2.2 - 0.1 per decade age
+
+*/
