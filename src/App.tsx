0c0f5dbff90516bf3434c57150cf52e137f6d071
danielmoi <dmoixy@gmail.com>
2023-10-11 16:11:04 +1100

    hack to move arrow up

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import References from './References';
 
 const Sticky = () => {
   return (
-    <div class="absolute bottom-0">
+    <div class="absolute bottom-2">
       <div class="text-lg">(more info)</div>
       <div>⬇</div>
     </div>
